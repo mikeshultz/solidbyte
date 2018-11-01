a37b41d9e0429d45e9ece1770938bc49b9ba154d
Mike Shultz <mike@lunyr.com>
2018-11-01 16:02:29 -0600

    Version bump to 0.1.0a1

--- solidbyte/__init__.py
+++ solidbyte/__init__.py
@@ -2,7 +2,7 @@ from .compile import Compiler
 from .common.metafile import MetaFile
 from .common.web3 import web3c
 
-__version__='0.0.3a2'
+__version__='0.1.0a1'
 __author__ = 'Mike Shultz'
 __copyright__ = "Copyright 2018, GoInto, LLC"
 __license__ = 'GPLv3'
