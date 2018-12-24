f2300e58cbe5980daea38d6a68d33cff3617bce6
Mike Shultz <shultzm@gmail.com>
2018-12-23 23:01:32 -0700

    Updated solc to 0.5.2

--- scripts/install_solc.sh
+++ scripts/install_solc.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-SOLC_VERSION="v0.4.25"
+SOLC_VERSION="v0.5.2"
 SOLC_DOWNLOAD="https://github.com/ethereum/solidity/releases/download/$SOLC_VERSION/solc-static-linux"
-SOLC_DEST="$(pwd)/bin/solc"
+SOLC_DEST="$(pwd)/solidbyte/bin/solc"
 
 echo "Downloading the Solidity compiler..."
 curl -sL $SOLC_DOWNLOAD --output $SOLC_DEST && chmod +x $SOLC_DEST
