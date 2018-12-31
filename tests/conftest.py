8c08d71e8e4319c0542ae55e830d5618ff4b2162
Mike Shultz <shultzm@gmail.com>
2018-12-30 23:57:42 -0700

    added mock_project pytest fixture as a generator to provide a new clean project and worked it into the test_testing integration test.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,22 @@
+import pytest
+from datetime import datetime
+from contextlib import contextmanager
 from solidbyte.common.logging import setDebugLogging
+from .const import TMP_DIR
+from .utils import create_mock_project, delete_path_recursively
 
 
 def pytest_sessionstart(session):
     """ before session.main() is called. """
     setDebugLogging()
+
+
+@pytest.fixture
+def mock_project():
+    @contextmanager
+    def yield_mock_project(tmpdir=TMP_DIR):
+        project_dir = tmpdir.joinpath('project-{}'.format(datetime.now().timestamp()))
+        create_mock_project(project_dir)
+        yield project_dir
+        delete_path_recursively(project_dir)
+    return yield_mock_project
