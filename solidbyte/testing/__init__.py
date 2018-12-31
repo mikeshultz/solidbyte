a604c4ddcb7929c51604849f640748ae435c1c05
Mike Shultz <shultzm@gmail.com>
2018-12-30 22:53:40 -0700

    added file locations and the ability to pass a web3 object to SolidbyteTestPlugin so it can be properly tested.

--- solidbyte/testing/__init__.py
+++ solidbyte/testing/__init__.py
@@ -7,8 +7,16 @@ from ..common.exceptions import SolidbyteException
 
 class SolidbyteTestPlugin(object):
 
-    def __init__(self, network_name):
+    def __init__(self, network_name, web3=None, project_dir=None, contract_dir=None,
+                 deploy_dir=None):
         self.network = network_name
+        self._web3 = None
+        if web3 is not None:
+            self._web3 = web3
+
+        self._project_dir = project_dir
+        self._contract_dir = contract_dir
+        self._deploy_dir = deploy_dir
 
     def pytest_sessionfinish(self):
         # TODO: There was something I wanted to do here...
@@ -16,9 +24,11 @@ class SolidbyteTestPlugin(object):
 
     @pytest.fixture
     def contracts(self):
-        web3 = web3c.get_web3(self.network)
-        network_id = web3.net.chainId or web3.net.version
-        d = Deployer(self.network)
+        if not self._web3:
+            self._web3 = web3c.get_web3(self.network)
+        network_id = self._web3.net.chainId or self._web3.net.version
+        d = Deployer(self.network, project_dir=self._project_dir, contract_dir=self._contract_dir,
+                     deploy_dir=self._deploy_dir)
         contracts_meta = d.deployed_contracts
         contracts_compiled = d.source_contracts
         test_contracts = {}
@@ -31,17 +41,30 @@ class SolidbyteTestPlugin(object):
                     )
             if latest is not None:
                 abi = contracts_compiled[meta['name']].abi
-                test_contracts[meta['name']] = web3.eth.contract(abi=abi, address=latest['address'])
+                test_contracts[meta['name']] = self._web3.eth.contract(
+                    abi=abi,
+                    address=latest['address']
+                )
         if len(test_contracts) == 0:
             raise SolidbyteException("No deployed contracts to test")
         return AttrDict(test_contracts)
 
     @pytest.fixture
     def web3(self):
-        web3 = web3c.get_web3(self.network)
-        return web3
+        if not self._web3:
+            self._web3 = web3c.get_web3(self.network)
+        return self._web3
 
 
-def run_tests(network_name):
+def run_tests(network_name, args=[], web3=None, project_dir=None, contract_dir=None,
+              deploy_dir=None):
     """ Run all tests on project """
-    pytest.main([], plugins=[SolidbyteTestPlugin(network_name=network_name)])
+    return pytest.main(args, plugins=[
+            SolidbyteTestPlugin(
+                network_name=network_name,
+                web3=web3,
+                project_dir=project_dir,
+                contract_dir=contract_dir,
+                deploy_dir=deploy_dir,
+            )
+        ])
