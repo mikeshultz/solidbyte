a35469ad20fab995e54b1fabaee979c9e798d196
Mike Shultz <shultzm@gmail.com>
2018-12-31 03:44:39 -0700

    expanded common utils tests

--- tests/test_common_utils.py
+++ tests/test_common_utils.py
@@ -29,19 +29,38 @@ def test_get_filename_and_ext():
     fname2, ext2 = get_filename_and_ext('multiple.dots.dat')
     assert fname2 == 'multiple.dots'
     assert ext2 == 'dat'
+    fname2, ext2 = get_filename_and_ext('nodots')
+    assert fname2 == 'nodots'
+    assert ext2 == ''
 
 
 def test_supported_extension():
     assert supported_extension('MyContract.sol')
+    assert not supported_extension('MyContract.cpp')
 
 
 def test_source_filename_to_name():
     name = source_filename_to_name('MyContract.sol')
     assert name == 'MyContract'
+    try:
+        name = source_filename_to_name('MyContract.cpp')
+        assert False, "Should have raised a ValueError"
+    except ValueError:
+        assert True
 
 
 def test_collapse_oel():
     assert 'one' == collapse_oel(['one'])
+    try:
+        collapse_oel('MyContract.sol')
+        assert False, "Should have raised a ValueError"
+    except ValueError:
+        assert True
+    try:
+        collapse_oel(['one', 'two'])
+        assert False, "Should have raised a ValueError"
+    except ValueError:
+        assert True
 
 
 def test_pop_key_from_dict():
@@ -61,6 +80,7 @@ def test_all_defs_in():
         'two': 2,
     }
     assert all_defs_in([('one', '-'), ('two', '-')], tdict)
+    assert not all_defs_in([('three', '-')], tdict)
 
 
 def test_defs_not_in():
