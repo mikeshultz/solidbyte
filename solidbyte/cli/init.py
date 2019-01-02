1294a33eac55303bc8381473d52d9ccefe439516
Mike Shultz <shultzm@gmail.com>
2019-01-01 23:55:53 -0700

    removed unnecessary CLI arg

--- solidbyte/cli/init.py
+++ solidbyte/cli/init.py
@@ -9,8 +9,6 @@ log = getLogger(__name__)
 
 def add_parser_arguments(parser):
     """ Add additional subcommands onto this command """
-    parser.add_argument('-m', action='store_true', default=False,
-                        dest='exclude_migration', help='Exclude migration junk')
     parser.add_argument('--dir-mode', type=str, dest='dir_mode',
                         help='Create directories with mode')
     parser.add_argument('-t', '--template', type=str, dest='template',
