86d2830aea60009c4c95229ae9979f0b05f69a69
Mike Shultz <shultzm@gmail.com>
2019-01-09 14:24:18 -0700

    Added functions to handle artifacts(bin and abi files generated by Compiler)

---
+++ solidbyte/compile/compiled.py
@@ -0,0 +1,87 @@
+import json
+from typing import Union, Optional, Any, Tuple, Dict, Set, List
+from pathlib import Path
+from attrdict import AttrDict
+from ..common.utils import to_path, to_path_or_cwd
+from ..common.exceptions import SolidbyteException
+from ..common.logging import getLogger
+
+log = getLogger(__name__)
+
+# Typing
+PS = Union[Path, str]
+
+# Module defs
+ARTIFACT_CACHE: Set[Any] = set()
+
+
+class CompiledContract:
+    def __init__(self, name: str, artifact_path: PS) -> None:
+        self.name = name
+        self.artifact_path: PS = to_path(artifact_path)
+        self.paths: AttrDict = AttrDict({
+            'abi': self.artifact_path.joinpath('{}.abi'.format(self.name)),
+            'bytecode': self.artifact_path.joinpath('{}.bin'.format(self.name)),
+        })
+
+        self.abi: Optional[Dict] = None
+        self.bytecode: Optional[str] = None
+
+        if not self._load_artifacts():
+            log.warning("Loading of {} artifacts failed.".format(self.name))
+
+    def _load_artifacts(self):
+        """ Load the artifact files """
+
+        # Load the bytecode
+        with self.paths.bytecode.open() as _file:
+            log.debug("Reading file {}...".format(self.paths.bytecode))
+            self.bytecode = _file.read()
+
+        # Load the ABI
+        with self.paths.abi.open() as _file:
+            log.debug("Reading file {}...".format(self.paths.abi))
+            abi_str = _file.read()
+            self.abi = json.loads(abi_str)
+
+        return self.abi and self.bytecode
+
+
+def available_contract_names(project_dir: PS) -> Set[str]:
+    """ Return the names of all compiled contracts """
+    project_dir = to_path_or_cwd(project_dir)
+
+    builddir = project_dir.joinpath('build')
+    if not builddir.is_dir():
+        raise SolidbyteException("My word.  The build directory appears to be missing.")
+
+    c_names = set()
+
+    for d in builddir.iterdir():
+        if d.is_dir():
+            c_names.add(d.name)
+
+    return c_names
+
+
+def contract_artifacts(project_dir: PS, name: str) -> CompiledContract:
+    """ Return a CompiledContract object with the artifacts for a contract """
+    project_dir = to_path_or_cwd(project_dir)
+    builddir = project_dir.joinpath('build')
+    artifact_path = builddir.joinpath(name)
+    cc = CompiledContract(name=name, artifact_path=artifact_path)
+    return cc
+
+
+def artifacts(project_dir: PS) -> Set[Any]:
+    """ Get the artifacts for all compiled contracts """
+
+    project_dir = to_path_or_cwd(project_dir)
+    contracts = available_contract_names(project_dir)
+
+    artifacts = set()
+
+    for contract in contracts:
+        artifacts.add(contract_artifacts(project_dir, contract))
+
+    return artifacts
---
+++ tests/test_compile_compiled.py
@@ -0,0 +1,66 @@
+""" Test the artifact functions and objects """
+from solidbyte.compile import Compiler
+from solidbyte.compile.compiled import (
+    artifacts,
+    contract_artifacts,
+    available_contract_names,
+    CompiledContract,
+)
+
+
+def test_CompiledContract(mock_project):
+    """ test the CompiledContract object """
+    with mock_project() as mock:
+        contract_name = 'Test'
+
+        compiler = Compiler(project_dir=mock.paths.project)
+        compiler.compile_all()
+
+        cc = CompiledContract(name=contract_name,
+                              artifact_path=mock.paths.build.joinpath(contract_name))
+        assert cc.abi
+        assert cc.bytecode
+
+
+def test_available_contract_names(mock_project):
+    """ test the available_contract_names function """
+    with mock_project() as mock:
+        contract_name = 'Test'
+
+        compiler = Compiler(project_dir=mock.paths.project)
+        compiler.compile_all()
+
+        names = available_contract_names(mock.paths.project)
+        assert contract_name in names
+
+
+def test_contract_artifacts(mock_project):
+    """ test the contract_artifacts function """
+    with mock_project() as mock:
+        contract_name = 'Test'
+
+        compiler = Compiler(project_dir=mock.paths.project)
+        compiler.compile_all()
+
+        cc = contract_artifacts(mock.paths.project, contract_name)
+        assert isinstance(cc, CompiledContract)
+        assert cc.abi
+        assert cc.bytecode
+
+
+def test_artifacts(mock_project):
+    """ test the artifacts function """
+    with mock_project() as mock:
+        contract_name = 'Test'
+
+        compiler = Compiler(project_dir=mock.paths.project)
+        compiler.compile_all()
+
+        all_artifacts = artifacts(mock.paths.project)
+        assert type(all_artifacts) == set
+        found = False
+        for cc in all_artifacts:
+            assert isinstance(cc, CompiledContract)
+            if cc.name == contract_name:
+                found = True
+        assert found
