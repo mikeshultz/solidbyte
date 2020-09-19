2d769b24019e7ee3147b43398707d0b19a81e153
Mike Shultz <shultzm@gmail.com>
2020-09-19 17:50:36 -0600

    add python 3.8

--- setup.py
+++ setup.py
@@ -98,6 +98,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     keywords='solidity ethereum development',
     packages=find_packages(exclude=['docs', 'tests', 'scripts', 'build']),
